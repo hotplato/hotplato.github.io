74feadab3ef3b49b87df535b3f60918371b71039
plato <feifeiwoye@outlook.com>
2024-03-31 00:50:49 +0800

    chore: 增加base64等

--- themes/simple/tailwind.config.js
+++ themes/simple/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.{html,js}"],
+  content: ["./layouts/**/*.{html,js}", "../../content/**/*.{html,js}"],
   theme: {
     extend: {},
   },
